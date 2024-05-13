5d8818e987cd86f55ed19e11cbf27764f530c2b3
akbarhd <akbarhossam123@gmail.com>
2024-05-13 10:02:58 +0700

    menmabahkan file tugas praktik 1 html css dan js

--- indexx.php
+++
@@ -1,7 +0,0 @@
-<nav>
-    <ul>
-        <li><a href="form-daftar-anggota.php">Daftar Baru</a></li>
-        <!-- tambahkan baris berikut ini -->
-        <li><a href="list-daftar-anggota_AkbarHossamDelmiro.php">List Anggota Perpus</a></li>
-    </ul>
-</nav>
