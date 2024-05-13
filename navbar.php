74916258c1c34088538030adb6ac71baec0581da
unknown <falihfauzan64@gmail.com>
2024-05-13 10:06:12 +0700

    Menghapus semua file

--- navbar.php
+++
@@ -1,5 +0,0 @@
-<nav class="navbar bg-body-tertiary">
-            <div class="container">
-                <span class="navbar-brand h1 mx-auto my-0">Tugas Akhir PHP dan MySQL</span>
-            </div>
-</nav>
