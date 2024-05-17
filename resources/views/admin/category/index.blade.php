7e59d2418b799b9cdcfb9e3db1b4047aed97e205
akbarhd <akbarhossam123@gmail.com>
2024-05-17 09:07:25 +0700

    upload laravel dashboard

---
+++ resources/views/admin/category/index.blade.php
@@ -0,0 +1,56 @@
+@extends('admin.layout.app')
+
+@section('title', 'Category')
+
+@section('after-style')
+    <link rel="stylesheet" href="{{ asset('assets/extensions/simple-datatables/style.css    ') }}">
+    <link rel="stylesheet" href="{{ asset('assets/compiled/css/table-datatable.css') }}">
+@endsection
+
+@include('admin.components.sidebar')
+
+@section('content')
+
+    <div class="page-heading">
+        <h3>Halaman Category</h3>
+    </div>
+
+    <div class="page-content">
+        <section class="section">
+            <div class="card">
+                <div class="card-header">
+                    <h5 class="card-title">
+                      Category
+                    </h5>
+                </div>
+                <div class="card-body">
+                    <table class="table table-striped" id="table1">
+                        <thead>
+                            <tr>
+                                <th>Category</th>
+                                <th>slug</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Web Developer</td>
+                                <td>web-developer</td>
+                                <td>
+                                    <a href="javascript:;" class="btn btn-primary "><i class="bi bi-plus-circle-dotted"></i></a>
+                                    <a href="javascript:;" class="btn btn-warning"> <i class="bi bi-pencil-square"></i></a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </section>
+    </div>
+@endsection
+
+@section('after-script')
+    <script src="assets/extensions/simple-datatables/umd/simple-datatables.js"></script>
+    <script src="assets/static/js/pages/simple-datatables.js"></script>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/admin', function () {
+    return view('admin.dashboard');
+});
+
+Route::resource('category', CategoryController::class);
+Route::resource('user', UserController::class);
