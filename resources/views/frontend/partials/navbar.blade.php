13307df215c60002c6d4e9d49c85e19f164bc700
Akbar Hossam Delmiro <akbarhossam123@gmail.com>
2024-05-31 00:04:52 +0700

    add navbar

--- resources/views/frontend/partials/navbar.blade.php
+++ resources/views/frontend/partials/navbar.blade.php
@@ -7,7 +7,8 @@
                     <div class="col-lg-3 col-md-2">
                         <!-- Logo -->
                         <div class="logo">
-                            <a href="javascript:;"><img src="assets/img/logo/logo.png" alt="logo" width="100" height="90"/></a>
+                            <a href="javascript:;"><img src="assets/img/logo/logo.png" alt="logo" width="100"
+                                    height="90" /></a>
                         </div>
                     </div>
                     <div class="col-lg-9 col-md-10">
@@ -15,33 +16,40 @@
                             <!-- Main-menu -->
                             <div class="main-menu" style="flex-grow: 1;">
                                 <nav class="d-none d-lg-block">
-                                    <ul id="navigation">
-                                        @guest
-                                            <li><a href="/">Beranda</a></li>
-                                            <li><a href="/job-listing">Cari Lowongan Kerja</a></li>
-                                            <li><a href="/about">Tentang</a></li>
-                                            <li><a href="/contact">Kontak</a></li>
-                                            <li><a href="/login">Login</a></li>
-                                        @endguest
-                                        @auth
-                                            <li><a href="/">Beranda</a></li>
-                                            <li><a href="/job-listing">Cari Lowongan Kerja</a></li>
-                                            <li><a href="/about">Tentang</a></li>
-                                            <li><a href="/contact">Kontak</a></li>
-                                            <li><a href="{{ route('dashboarduser') }}">Dashboard</a></li>
-                                            <li><a href="{{ route('coba2') }}">Coba2</a></li>
-                                            <li>
-                                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                                    class="d-none">
-                                                    @csrf
-                                                </form>
-                                                <a class="sidebar-link" href="{{ route('logout') }}"
-                                                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                                    <i class="fa-solid fa-right-from-bracket"></i>
-                                                    <span>Logout</span>
-                                                </a>
-                                            </li>
-                                        @endauth
+                                    <ul id="navigation" style="">
+                                        <span style="">
+                                            <li><a class="{{ Route::currentRouteName() === 'home' ? 'text-danger' : '' }}"
+                                                    href="/">Beranda</a></li>
+                                            <li><a class="{{ Route::currentRouteName() === 'job-listing' ? 'text-danger' : '' }}"
+                                                    href="/job-listing">Cari Pekerjaan</a></li>
+                                            <li><a class="{{ Route::currentRouteName() === 'about' ? 'text-danger' : '' }}"
+                                                    href="{{ route('about') }}">Tentang</a></li>
+                                            <li><a class="{{ Route::currentRouteName() === 'contact' ? 'text-danger' : '' }}"
+                                                    href="{{ route('contact') }}">Kontak</a></li>
+                                        </span>
+                                        <span>
+                                            @guest
+                                                <li><a href="{{ route('login') }}" class="login-btn mb-3 mb-lg-0">Masuk</a>
+                                                </li>
+                                                <li><a href="{{ route('register') }}" class="register-btn">Daftar</a></li>
+                                            @endguest
+                                            @auth
+                                                <li><a href="{{ route('dashboarduser') }}"
+                                                        class="dashboard-btn mt-lg-2 mb-2">Dashboard <i
+                                                            class="fa-solid fa-address-book"></i></a>
+                                                </li>
+                                                <li>
+                                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                                        class="d-none">
+                                                        @csrf
+                                                    </form>
+                                                    <a class="logout-btn" href="{{ route('logout') }}"
+                                                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                                        Keluar <i class="fas fa-sign-out-alt"></i>
+                                                    </a>
+                                                </li>
+                                            @endauth
+                                        </span>
                                     </ul>
                                 </nav>
                             </div>
